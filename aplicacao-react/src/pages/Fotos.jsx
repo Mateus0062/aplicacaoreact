fcb02856f1b71057aab11b6a1a776d93581e76ca
Mateus0062 <mateusmendmattos@gmail.com>
2024-10-03 23:38:58 -0300

    Fotos

--- aplicacao-react/src/pages/Fotos.jsx
+++ aplicacao-react/src/pages/Fotos.jsx
@@ -1,13 +1,34 @@
-import ImageSlider from './ImageSlider';
+import React, { useState } from 'react';
+import '../Css/Fotos.css'
 
-const Fotos = () => {
+const Fotos = ({ images }) => {
+    const [current, setCurrent] = useState(0);
+    const length = images.length;
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+
+    if (!Array.isArray(images) || images.length <= 0) {
+        return null;
+    }
+
     return (
-        <div>
-            <h1>Fotos Page</h1>
-            <p>Welcome to the Fotos page!</p>
-            <ImageSlider>
-            </ImageSlider>
-        </div>
+        <section className='slider'>
+            {images.map((image, index) => (
+                <div key={index} className={index === current ? 'slide active' : 'slide'}>
+                    {index === current && (
+                        <img src={image} alt='travel image' className='image' />
+                    )}
+                </div>
+            ))}
+            <button className='left-arrow' onClick={prevSlide}>Prev</button>
+            <button className='right-arrow' onClick={nextSlide}>Next</button>
+        </section>
     );
 };
 
