f1b4c07e1d72374c0fa491bb1e39536fde3b79c5
Mateus0062 <mateusmendmattos@gmail.com>
2024-10-04 14:01:02 -0300

    Fotos

--- aplicacao-react/src/pages/Fotos.jsx
+++ aplicacao-react/src/pages/Fotos.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Fotos = () => {
-    return (
-        <div>
-            <h1>Fotos Page</h1>
-            <p>Welcome to the Fotos page!</p>
-        </div>
-    );
+    
+    const [photos, setPhotos] = useState([]);
+
+    const handlePhoto = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const newPhoto = URL.createObjectURL(file);
+            setPhotos((prevPhotos) => [..prevPhotos, newPhoto]);
+        }
+    }
+    
+  return (
+    <div>
+      <h1>Photo Library</h1>
+      
+      {/* Formulário para adicionar novas fotos */}
+      <input type="file" accept="image/*" onChange={handleAddPhoto} />
+      
+      {/* Exibindo as fotos */}
+      <div style={{ display: 'flex', flexWrap: 'wrap', marginTop: '20px' }}>
+        {photos.map((photo, index) => (
+          <div key={index} style={{ margin: '10px' }}>
+            <img src={photo} alt={`Photo ${index + 1}`} style={{ width: '150px', height: '150px', objectFit: 'cover' }} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Fotos;
