7a740d42409c79e0ae50682d9bf94b0eb71ecf72
Mateus0062 <mateusmendmattos@gmail.com>
2024-10-04 13:52:40 -0300

    Fotos

--- aplicacao-react/src/pages/Fotos.jsx
+++ aplicacao-react/src/pages/Fotos.jsx
@@ -1,35 +1,11 @@
-/* eslint-disable react/prop-types */
-import { useState } from 'react';
-import '../Css/Fotos.css'
+import React from 'react';
 
-const Fotos = ({ images }) => {
-    const [current, setCurrent] = useState(0);
-    const lenght = images.lenght;
-
-    const nextSlide = () => {
-        setCurrent(current === lenght - 1 ? 0 : current + 1);
-    };
-
-    const prevSlide = () => {
-        setCurrent(current === 0 ? lenght - 1 : current - 1);
-    }
-
-    if (!Array.isArray(images) || images.length <= 0) {
-        return null;
-    }
-
+const Fotos = () => {
     return (
-        <section className='slider'>
-            {images.map((image, index) => (
-                <div key={index} className={index === current ? 'slide active' : 'slide'}>
-                    {index === current && (
-                        <img src={image} alt='travel image' className='image' />
-                    )}
-                </div>
-            ))}
-            <button className='left-arrow' onClick={prevSlide}>Prev</button>
-            <button className='right-arrow' onClick={nextSlide}>Next</button>
-        </section>
+        <div>
+            <h1>Contagem Page</h1>
+            <p>Welcome to the Contagem page!</p>
+        </div>
     );
 };
 
