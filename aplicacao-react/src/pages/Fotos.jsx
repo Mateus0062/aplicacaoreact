e875fe05249956507f4191c54b684a9005ec859f
Mateus0062 <mateusmendmattos@gmail.com>
2024-10-04 13:53:48 -0300

    Fotos

--- aplicacao-react/src/pages/Fotos.jsx
+++ aplicacao-react/src/pages/Fotos.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Fotos = () => {
     return (
         <div>
-            <h1>Contagem Page</h1>
-            <p>Welcome to the Contagem page!</p>
+            <h1>Fotos Page</h1>
+            <p>Welcome to the Fotos page!</p>
         </div>
     );
 };
